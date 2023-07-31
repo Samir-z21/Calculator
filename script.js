555fa628e193b7d4c04e9fa4161499b9f89bbd9c
Samir <samir_zulfiqar@yahoo.com>
2023-07-31 14:57:29 -0400

    made the display of the calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+function addition (a,b) {
+    return a+b
+}
+
+function substraction (a,b){
+    return a-b
+}
+
+function multiplication (a,b){
+    return a * b
+}
+
+function division (a,b) {
+    return a / b
+}
+
+console.log(addition(8,2))
+console.log(substraction(8,2))
+console.log(multiplication(8,2))
+console.log(division(8,2))
