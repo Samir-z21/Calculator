dc9c7722c400f07e30b2c6861146a5bb6895e4f4
Samir <samir_zulfiqar@yahoo.com>
2023-08-01 12:02:15 -0400

    coded the DOM 'spans' to show dynamically on the screen

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+let screen = document.querySelector("#screen");
+let variableA = document.createElement('span');
+let variableB = document.createElement('span');
+let Operator = document.createElement('span');
+
+let variables = document.querySelectorAll(".variables");
+variables.forEach((variable) => {
+
+    variable.addEventListener('click', function(e){
+        if (Operator && Operator.parentNode === screen) return;
+            variableA.textContent += e.target.innerText;
+            screen.appendChild(variableA);
+});
+
+
+    variable.addEventListener('click', function(e){
+        if (Operator && Operator.parentNode === screen) {
+            variableB.textContent += e.target.innerText;
+            screen.appendChild(variableB);
+        };
+});
+});
+
+
+let operators = document.querySelectorAll(".operators");
+operators.forEach((operator) => {
+operator.addEventListener('click', function(e){
+    if (Operator && Operator.parentNode === screen) return
+    Operator.textContent = e.target.innerText;
+    screen.appendChild(Operator);
+});
+});
+
+function operation (Operator,variableA,variableB) {
+    
+}
+
+
 function addition (a,b) {
     return a+b
 }
@@ -13,8 +51,3 @@ function multiplication (a,b){
 function division (a,b) {
     return a / b
 }
-
-console.log(addition(8,2))
-console.log(substraction(8,2))
-console.log(multiplication(8,2))
-console.log(division(8,2))
